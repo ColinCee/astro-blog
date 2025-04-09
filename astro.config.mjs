6f8078d83610d49096077071ce2badff1d62d5c0
Colin Cheung <ColinCee@users.noreply.github.com>
2025-04-09 10:59:25 +0100

    Initial blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import expressiveCode from 'astro-expressive-code';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [expressiveCode({
+    // Try changing the theme(s) here:
+    themes: ['github-dark'], // Example: Try this if you were using another
+    // ... other options
+  }),mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
