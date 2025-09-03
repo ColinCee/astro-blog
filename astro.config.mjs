8d2ed5a6b04c53d5fa3cdc3c7b326ed6d8c070be
Colin Cheung <ColinCee@users.noreply.github.com>
2025-09-04 00:27:25 +0100

    INIT files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,26 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import expressiveCode from 'astro-expressive-code';
 import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [expressiveCode({
-    // Try changing the theme(s) here:
-    themes: ['github-dark'], // Example: Try this if you were using another
-    // ... other options
-  }),mdx(), sitemap()],
+  integrations: [
+    expressiveCode({
+      // Try changing the theme(s) here:
+      themes: ['github-dark'], // Example: Try this if you were using another
+      // ... other options
+    }),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }), 
+    sitemap(),
+    react()
+  ],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
